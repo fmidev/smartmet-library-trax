f2aecec07021c5feee548c1e98da45ebd5d7026c
Mika Heiskanen <mika.heiskanen@fmi.fi>
2023-07-24 18:32:50 +0300

    Silenced compiler warnings

--- trax/Endian.cpp
+++ trax/Endian.cpp
@@ -7,6 +7,7 @@ unsigned char byteorder()
 {
   unsigned char byteOrder = 1;
   std::uint16_t word = 1;
+  // codechecker_false_positive [knownConditionTrueFalse] This is a well known idiom
   if (*reinterpret_cast<std::uint8_t*>(&word) == 0)
     byteOrder = 1;
   return byteOrder;
